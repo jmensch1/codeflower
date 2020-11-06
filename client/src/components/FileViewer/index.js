87dbd848866c649f582268bf3d37beec94a58d29
Jake Mensch <jake.mensch@gmail.com>
2020-11-06 10:01:30 -0800

    updatd test-repo-2

--- client/src/components/FileViewer/index.js
+++ client/src/components/FileViewer/index.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import { useFiles } from 'store/selectors'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  root: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    height: 600,
+    width: 400,
+    border: '1px black solid',
+    padding: 10,
+    zIndex: 1,
+    overflow: 'auto',
+    backgroundColor: 'white',
+    '& pre': {
+      whiteSpace: 'pre-wrap',
+      wordWrap: 'break-word',
+      fontSize: 12,
+    }
+  }
+})
 
 const FileViewer = () => {
-  const files = useFiles()
-  const file = files.selectedFile
-    ? files.files[files.selectedFile]
-    : null
+  const classes = useStyles()
+  const { files, selectedFile }= useFiles()
+  const file = selectedFile ? files[selectedFile] : null
 
+  if (!file) return null
   return (
-    <div
-      style={{
-        position: 'absolute',
-        bottom: 10,
-        left: 10,
-        height: 600,
-        width: 400,
-        border: '1px black solid',
-        padding: 10,
-        zIndex: 1,
-        overflow: 'auto'
-      }}
-    >
-      <pre
-        style={{
-          //overflow: 'auto',
-          whiteSpace: 'pre-wrap',
-          wordWrap: 'break-word',
-        }}
-      >
-        { file }
-      </pre>
+    <div className={classes.root}>
+      <pre><code>{ file }</code></pre>
     </div>
   )
 }
