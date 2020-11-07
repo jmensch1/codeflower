c12d445bc1847280a737ffa9f6729f28456a8afc
Jake Mensch <jake.mensch@gmail.com>
2020-11-06 21:09:46 -0800

    env var name

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.API_URL || 'http://localhost:8080'
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080'
 
 export const getRepo = async ({ owner, name, branch }) => {
   const { data } = await axios.post(`${BASE_URL}/cloc`, { owner, name, branch})
