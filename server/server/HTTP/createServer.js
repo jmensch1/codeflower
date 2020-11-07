53363bf50dc73fb6c493ef0790723d72b191b8f0
Jake Mensch <jake.mensch@gmail.com>
2020-11-07 04:41:25 -0800

    using express/cors

--- server/server/HTTP/createServer.js
+++ server/server/HTTP/createServer.js
@@ -1,24 +1,36 @@
-////////////////////// IMPORTS /////////////////////////
+// ////////////////////// IMPORTS /////////////////////////
+//
+// const http = require('http'),
+//       https = require('https'),
+//       fs = require('fs'),
+//       config = require('@config');
+//
+// ////////////////////// PRIVATE /////////////////////////
+//
+// let createServer;
+//
+// if (config.protocols.HTTP === 'https')
+//   createServer = (server) => {
+//     return https.createServer({
+//       key:  fs.readFileSync(config.paths.SSL.key,  'utf8'),
+//       cert: fs.readFileSync(config.paths.SSL.cert, 'utf8')
+//     }, server);
+//   };
+// else
+//   createServer = http.createServer;
+//
+// ////////////////////// EXPORTS /////////////////////////
+//
+// module.exports = createServer;
 
-const http = require('http'),
-      https = require('https'),
-      fs = require('fs'),
-      config = require('@config');
+const express = require('express')
+const cors = require('cors')
+const config = require('@config')
 
-////////////////////// PRIVATE /////////////////////////
-
-let createServer;
-
-if (config.protocols.HTTP === 'https')
-  createServer = (server) => {
-    return https.createServer({
-      key:  fs.readFileSync(config.paths.SSL.key,  'utf8'),
-      cert: fs.readFileSync(config.paths.SSL.cert, 'utf8')
-    }, server);
-  };
-else
-  createServer = http.createServer;
-
-////////////////////// EXPORTS /////////////////////////
-
-module.exports = createServer;
+module.exports = (server) => {
+  const app = express()
+  app.use(express.json())
+  app.use(cors())
+  app.post('*', server)
+  return app
+}
--- server/server/HTTP/parseRequest.js
+++ server/server/HTTP/parseRequest.js
@@ -8,40 +8,45 @@ const config = require('@config'),
 
 function parseRequest(request) {
   return new Promise((resolve, reject) => {
-
-    let urlInfo;
-    try {
-      urlInfo = url.parse(request.url, true);
-    } catch(e) {
-      reject(config.errors.ParseError);
-    }
-
-    let reqInfo = {
+    resolve({
       method: request.method,
-      endpoint: urlInfo.pathname.substring(1)
-    };
-
-    switch(request.method) {
-      case 'GET':
-        reqInfo.params = urlInfo.query;
-        resolve(reqInfo);
-        break;
-      case 'POST':
-        let body = '';
-        request.on('data', data => body += data);
-        request.on('end', () => {
-          try {
-            reqInfo.params = JSON.parse(body);
-          } catch(e) {
-            reject(config.errors.ParseError);
-          }
-          resolve(reqInfo);
-        });
-        break;
-      default:
-        reject(config.errors.MethodNotAllowed);
-        break;
-    }
+      endpoint: request.path.substring(1),
+      params: request.body,
+    })
+    //
+    // let urlInfo;
+    // try {
+    //   urlInfo = url.parse(request.url, true);
+    // } catch(e) {
+    //   reject(config.errors.ParseError);
+    // }
+    //
+    // let reqInfo = {
+    //   method: request.method,
+    //   endpoint: urlInfo.pathname.substring(1)
+    // };
+    //
+    // switch(request.method) {
+    //   case 'GET':
+    //     reqInfo.params = urlInfo.query;
+    //     resolve(reqInfo);
+    //     break;
+    //   case 'POST':
+    //     let body = '';
+    //     request.on('data', data => body += data);
+    //     request.on('end', () => {
+    //       try {
+    //         reqInfo.params = JSON.parse(body);
+    //       } catch(e) {
+    //         reject(config.errors.ParseError);
+    //       }
+    //       resolve(reqInfo);
+    //     });
+    //     break;
+    //   default:
+    //     reject(config.errors.MethodNotAllowed);
+    //     break;
+    // }
   });
 }
 
