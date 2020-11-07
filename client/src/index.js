3b59f98ccd355ea2c419e22bf71661ef879e4499
Jake Mensch <jake.mensch@gmail.com>
2020-11-06 20:55:29 -0800

    logging process

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import store from './store'
 //import reportWebVitals from './reportWebVitals'
 
+console.log(process.env)
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
