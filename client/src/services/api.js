e3b2ec85b79811b90887b6a6416f60a6e49af13e
Jake Mensch <jake.mensch@gmail.com>
2020-11-08 05:40:55 -0800

    consuming cloc view websockets

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,48 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080'
+const HTTP_URL = process.env.REACT_APP_API_URL_HTTP || 'http://localhost:8000'
+const WS_URL = process.env.REACT_APP_API_URL_WS || 'ws://localhost:8000'
 
+// HTTP VERSION
+// export const getRepo = async ({ owner, name, branch }) => {
+//   const { data } = await axios.post(`${HTTP_URL}/cloc`, { owner, name, branch})
+//   return data.data
+// }
+
+// WS VERSION
 export const getRepo = async ({ owner, name, branch }) => {
-  const { data } = await axios.post(`${BASE_URL}/cloc`, { owner, name, branch})
-  return data.data
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket(WS_URL)
+
+    socket.onopen = (e) => {
+      socket.send(JSON.stringify({
+        endpoint: 'cloc',
+        params: {
+          owner,
+          name,
+          branch,
+        }
+      }))
+    }
+
+    socket.onmessage = (e) => {
+      const { type, data } = JSON.parse(e.data)
+      switch(type) {
+        case 'success': return resolve(data)
+        case 'error':   return reject(data)
+        case 'update':  return console.log(data.text)
+        default:        return null
+      }
+    }
+
+    socket.onerror = (err) => {
+      console.log('WS connection error:', err)
+      reject(err)
+    }
+  })
 }
 
 export const getFile = async ({ repoId, name, path }) => {
-  const { data } = await axios.post(`${BASE_URL}/file`, { repoId, name, path })
+  const { data } = await axios.post(`${HTTP_URL}/file`, { repoId, name, path })
   return data.data
 }
