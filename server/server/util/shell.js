8b50682b21ca2ebb7d31e3f4e20388a99e52ff7b
Jake Mensch <jake.mensch@gmail.com>
2020-11-09 15:54:06 -0800

    created util/shell

---
+++ server/server/util/shell.js
@@ -0,0 +1,28 @@
+const { exec } = require('child_process')
+
+function execCmd(cmd, options={}) {
+  const { onUpdate, ...opts } = options
+
+  return new Promise((resolve, reject) => {
+    const proc = exec(cmd, opts, (err, stdout, stderr) => {
+      if (err)
+        reject({ err, stdout, stderr })
+      else
+        resolve({ stdout, stderr })
+    })
+
+    if (typeof onUpdate === 'function') {
+      proc.stdout.on('data', onUpdate)
+      proc.stderr.on('data', onUpdate)
+    }
+  })
+}
+
+function concat(lines) {
+  return lines.filter(line => line !== null).join(' ')
+}
+
+module.exports = {
+  exec: execCmd,
+  concat,
+}
