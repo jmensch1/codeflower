afff87f62856486b9fee5c986b02362fc07aeb59
Jake Mensch <jake.mensch@gmail.com>
2020-11-13 18:18:41 -0800

    add close button to FileViewer; transition experiments

--- client/src/components/FileViewer/index.js
+++ client/src/components/FileViewer/index.js
@@ -3,7 +3,27 @@ import { useDispatch } from 'react-redux'
 import { useFiles } from 'store/selectors'
 import { closeFile } from 'store/files'
 import { makeStyles } from '@material-ui/core/styles'
-import { Dialog, DialogTitle, DialogContent, CircularProgress } from '@material-ui/core'
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  CircularProgress,
+  Typography,
+  IconButton,
+
+  // transitions
+  // Slide,
+  Zoom,
+  // Grow,
+  // Collapse,
+  // Fade,
+} from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Zoom ref={ref} {...props} />
+  // return <Slide direction='up' ref={ref} {...props} />
+})
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,6 +46,12 @@ const useStyles = makeStyles(theme => ({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
   }
 }))
 
@@ -39,23 +65,30 @@ const FileViewer = () => {
 
   return (
     <Dialog
-      classes={classes}
+      className={classes.root}
       onClose={handleClose}
       open={!!file || !!error || isLoading}
       fullWidth
+      TransitionComponent={Transition}
     >
-      <DialogTitle onClose={handleClose}>
-        { selectedFile }
+      <DialogTitle>
+        <Typography variant='h6' component='div'>{ selectedFile }</Typography>
+        <IconButton
+          aria-label='close'
+          className={classes.closeButton}
+          onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
       </DialogTitle>
       <DialogContent dividers>
         {
           isLoading
           ? (
             <div className={classes.progress}>
-              <CircularProgress color='text' />
+              <CircularProgress color='inherit' />
             </div>
           )
-          : <pre><code>{ file || error && error.message }</code></pre>
+          : <pre><code>{ file || (error && error.message) }</code></pre>
         }
       </DialogContent>
     </Dialog>
