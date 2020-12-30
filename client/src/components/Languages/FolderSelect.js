6e2c360601a2768546adf0da7159b204482a7389
Jake Mensch <jake.mensch@gmail.com>
2020-12-30 07:53:46 -0800

    maxNodes in FolderSelect

--- client/src/components/Languages/FolderSelect.js
+++ client/src/components/Languages/FolderSelect.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { useFolders } from 'store/selectors'
 import { selectFolder } from 'store/folders'
 import Select from 'components/core/Select'
+import { MAX_NODES } from 'constants.js'
 
 const FolderSelect = () => {
   const folders = useFolders()
   const dispatch = useDispatch()
+
+  const options = useMemo(() => {
+    if (!folders || !folders.selectedFolder) return null
+
+    return folders.folderPaths.map(({ pathName, totalNodes }) => (
+      totalNodes <= MAX_NODES ? pathName : `${pathName} (${totalNodes} nodes)`
+    ))
+  }, [folders])
+
+  const onChange = useCallback((e) => {
+    dispatch(selectFolder(e.target.value))
+  }, [dispatch])
 
   if (!folders || !folders.selectedFolder) return null
 
@@ -14,8 +27,8 @@ const FolderSelect = () => {
     <div style={{ margin: 10 }}>
       <Select
         value={folders.selectedFolder}
-        onChange={e => dispatch(selectFolder(e.target.value))}
-        options={folders.folderPaths.map(({ pathName }) => pathName)}
+        onChange={onChange}
+        options={options}
       />
     </div>
   )
