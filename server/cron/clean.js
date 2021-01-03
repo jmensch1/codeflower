72b9919f7f508dbe995f96a0fd9bf27dd35aabdc
Jake Mensch <jake.mensch@gmail.com>
2021-01-03 15:32:45 -0800

    cron setup

--- server/cron/clean.js
+++ server/cron/clean.js
@@ -9,10 +9,19 @@ const rimraf = require('rimraf')
 
 const MAX_AGE = 24 * 60 * 60 * 1000
 
+console.log('running cron script')
+
 const { repos } = config.paths
-fs.readdirSync(repos).forEach(name => {
-  const absPath = path.join(repos, name)
-  const { birthtimeMs } = fs.statSync(absPath)
-  const age = Date.now() - birthtimeMs
-  if (age > MAX_AGE) rimraf.sync(absPath)
-})
+
+if (fs.existsSync(repos))
+  fs.readdirSync(repos).forEach(name => {
+    const absPath = path.join(repos, name)
+    const { birthtimeMs } = fs.statSync(absPath)
+    const age = Date.now() - birthtimeMs
+    if (age > MAX_AGE) {
+      console.log('deleting:', absPath)
+      rimraf.sync(absPath)
+    }
+  })
+
+console.log('finished cron script')
