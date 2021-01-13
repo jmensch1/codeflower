443078773b4d4d7ca97c8a2e27dc8d336b0d54e0
Jake Mensch <jake.mensch@gmail.com>
2021-01-12 16:25:45 -0800

    refactor checkRepoClonability

--- server/server/api/endpoints/cloc/index.js
+++ server/server/api/endpoints/cloc/index.js
@@ -11,23 +11,44 @@ const {
   sendJsonToClient,
   handleClocErrors,
 } = require('./subtasks/')
+const connPool = require('@util/connectionPool')(process.pid)
+const config = require('@config')
 
 //////////////// PRIVATE ///////////////////
 
-function serveClocData({ owner, name, branch, username, password }, onUpdate) {
-  const { uid, repo, creds } = processRequestParams({
-    owner,
-    name,
-    branch,
-    username,
-    password,
-  })
-  
-  const ctrl = { uid, repo, creds, onUpdate }
+async function serveClocData({ owner, name, branch, username, password }, onUpdate) {
+  const uid = connPool.addConn()
+  const fullName = `${owner}/${name}`
+  const fNameBr = fullName + (branch ? `::${branch}` : '')
+  const repoId = fullName.replace('/', '#') + '#' + uid
+  const creds = {
+    username: username && encodeURIComponent(username),
+    password: password && encodeURIComponent(password),
+  }
 
-  return checkRepoClonability(ctrl)
-    .then(checkRepoClonability)
-    .then(cloneRepoInFilesystem)
+  if (!owner || !name) throw config.errors.NeedOwnerAndName
+
+  const branches = await checkRepoClonability({ owner, name, creds, onUpdate })
+
+  if (branch && !Object.keys(branches).includes(branch))
+    throw config.errors.BranchNotFound
+
+  const ctrl = {
+    uid,
+    repo: {
+      owner,
+      name,
+      branch,
+      fullName,
+      fNameBr,
+      repoId,
+      branches,
+    },
+    creds,
+    onUpdate
+  }
+
+  return cloneRepoInFilesystem(ctrl)
     .then(getBranchNameIfNeeded)
     .then(convertRepoToClocFile)
     .then(getUsersJson)
--- server/server/api/endpoints/cloc/subtasks/02_checkRepoClonability.js
+++ server/server/api/endpoints/cloc/subtasks/02_checkRepoClonability.js
@@ -1,8 +1,8 @@
 //////////// IMPORTS ////////////
 
-const { exec, concat } = require('@util/shell'),
-  config = require('@config'),
-  Log = require('@log')
+const { exec, concat } = require('@util/shell')
+const config = require('@config')
+const Log = require('@log')
 
 //////////// PRIVATE //////////////
 
@@ -25,47 +25,39 @@ function getBranches(lsRemoteOutput) {
 // (2) whether the branch exists
 // (3) whether the repo requires credentials
 // (4) whether the credentials, if given, are valid
-function checkRepoClonability(ctrl) {
-  return new Promise((resolve, reject) => {
-    Log(2, '2. Checking Repo Clonability')
+async function checkRepoClonability({ owner, name, creds, onUpdate }) {
+  Log(2, '2. Checking Repo Clonability')
 
-    // construct the git ls-remote command.
-    // using '******' as a fallback is a trick that lets us
-    // distinguish between invalid credentials and non-existent repos,
-    // without triggering a username/password prompt
-    const user = ctrl.creds.username || '******'
-    const pass = ctrl.creds.password || '******'
-    const fName = ctrl.repo.fullName
-    const lsRemote = concat([
-      'git ls-remote',
-      `-h "https://${user}:${pass}@github.com/${fName}"`,
-    ])
+  // construct the git ls-remote command.
+  // using '******' as a fallback is a trick that lets us
+  // distinguish between invalid credentials and non-existent repos,
+  // without triggering a username/password prompt
+  const user = creds.username || '******'
+  const pass = creds.password || '******'
+  const lsRemote = concat([
+    'git ls-remote',
+    `-h "https://${user}:${pass}@github.com/${owner}/${name}"`,
+  ])
 
-    // echo the command (but not credentials)
-    ctrl.onUpdate('>> ' + lsRemote.replace(/\/.*?@/, '//******:******@'))
+  // echo the command (but not credentials)
+  onUpdate('>> ' + lsRemote.replace(/\/.*?@/, '//******:******@'))
 
-    return exec(lsRemote, { onUpdate: ctrl.onUpdate })
-      .then(({ stdout }) => {
-        // no error if the repo exists and the credentials are correct (if required)
-        ctrl.repo.branches = getBranches(stdout)
-        let { branch } = ctrl.repo
-        if (branch && Object.keys(ctrl.repo.branches).indexOf(branch) === -1)
-          reject(config.errors.BranchNotFound)
-        else resolve(ctrl)
-      })
-      .catch(({ err, stderr }) => {
-        // err happens if the credentials are wrong or the repo doesn't exist
-        // Repository not found => credentials are correct AND repository does not exist
-        // Invalid username or password => credentials are not correct AND repository may or may not exist
-        if (stderr.match(/Invalid username or password/))
-          if (ctrl.creds.username && ctrl.creds.password)
-            reject(config.errors.CredentialsInvalid)
-          else reject(config.errors.NeedCredentials)
-        else if (stderr.match(/Repository not found/))
-          reject(config.errors.RepoNotFound)
-        else reject(new Error(err))
-      })
-  })
+  try {
+    const { stdout } = await exec(lsRemote, { onUpdate })
+    return getBranches(stdout)
+  } catch ({ err, stderr }) {
+    // err happens if the credentials are wrong or the repo doesn't exist
+    // Repository not found => credentials are correct AND repository does not exist
+    // Invalid username or password => credentials are not correct AND repository may or may not exist
+    if (stderr.match(/Invalid username or password/))
+      throw creds.username && creds.password
+        ? config.errors.CredentialsInvalid
+        : config.errors.NeedCredentials
+    else if (stderr.match(/Repository not found/))
+      throw config.errors.RepoNotFound
+    else
+      throw err
+  }
 }
 
 //////////// EXPORTS //////////////
