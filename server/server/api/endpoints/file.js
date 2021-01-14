85b85a47aef4f1a2f65286216818eafbfe073b66
Jake Mensch <jake.mensch@gmail.com>
2021-01-13 21:43:47 -0800

    fix files endpoint

--- server/server/api/endpoints/file.js
+++ server/server/api/endpoints/file.js
@@ -4,7 +4,7 @@ const config = require('@config')
 module.exports = ({ repoId, path }) => {
   if (!repoId || !path) throw config.errors.BadFileRequest
 
-  const absPath = `${config.paths.repo(repoId)}/repo/${path}`
+  const absPath = `${config.paths.repo(repoId)}/root/${path}`
   return fs.promises.readFile(absPath, 'utf-8').catch((err) => {
     if (err.code === 'ENOENT') throw config.errors.FileNotFound
     else throw err
