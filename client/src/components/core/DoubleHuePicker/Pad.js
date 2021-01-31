b3753980840affe505d0224f6db7b88d17e5f01b
Jake Mensch <jake.mensch@gmail.com>
2021-01-31 10:24:24 -0800

    DoubleHuePicker mostly working

---
+++ client/src/components/core/DoubleHuePicker/Pad.js
@@ -0,0 +1,164 @@
+import React, { useEffect, useState, useCallback } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import * as d3 from 'd3'
+
+const CIRCLE_RADIUS = 10
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    position: 'relative',
+    '& > svg': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      height: '100%',
+      width: '100%',
+      '& > line': {
+        stroke: 'black',
+        strokeWidth: 3,
+        cursor: 'move',
+      },
+      '& > circle': {
+        fill: 'transparent',
+        stroke: 'black',
+        strokeWidth: 1,
+        cursor: 'ew-resize',
+      }
+    }
+  }
+}))
+
+function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max)
+}
+
+function interpolate(num, numRange, targetRange) {
+  const numRangeRatio = (num - numRange[0]) / (numRange[1] - numRange[0])
+  const targetValue = numRangeRatio * (targetRange[1] - targetRange[0]) + targetRange[0]
+  return clamp(targetValue, targetRange[0], targetRange[1])
+}
+
+const Pad = ({ color, onChange, hueRange, lightnessRange }) => {
+  const classes = useStyles()
+  const [dimensions, setDimensions] = useState(null)
+  const [circle0, setCircle0] = useState(null)
+  const [circle1, setCircle1] = useState(null)
+  const [bar, setBar] = useState(null)
+
+  const getHue = useCallback((x) => {
+    if (!hueRange || !dimensions) return null
+    return interpolate(x, [0, dimensions.width], hueRange)
+  }, [hueRange, dimensions])
+
+  const getX = useCallback((hue) => {
+    if (!hueRange || !dimensions) return null
+    return interpolate(hue, hueRange, [0, dimensions.width])
+  }, [hueRange, dimensions])
+
+  const getLightness = useCallback((y) => {
+    if (!lightnessRange || !dimensions) return null
+    return interpolate(y, [0, dimensions.height], lightnessRange)
+  }, [lightnessRange, dimensions])
+
+  const getY = useCallback((lightness) => {
+    if (!lightnessRange || !dimensions) return null
+    return interpolate(lightness, lightnessRange, [0, dimensions.height])
+  }, [lightnessRange, dimensions])
+
+  useEffect(() => {
+    const container = document.getElementById('double-hue-picker-container')
+
+    setDimensions({
+      width: container.offsetWidth,
+      height: container.offsetHeight,
+    })
+
+    const svg = d3
+      .select(container)
+      .append('svg')
+
+    const circle0 = svg
+      .append('circle')
+      .attr('r', CIRCLE_RADIUS)
+
+    const circle1 = svg
+      .append('circle')
+      .attr('r', CIRCLE_RADIUS)
+
+    const bar = svg
+      .append('line')
+
+    setCircle0(circle0)
+    setCircle1(circle1)
+    setBar(bar)
+
+    return () => {
+      container.innerHTML = ''
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!circle0) return
+
+    circle0.call(
+      d3
+        .drag()
+        .on('drag', ({ x, y }) => {
+          onChange((color) => ({
+            ...color,
+            hue: [getHue(x), color.hue[1]]
+          }))
+        })
+    )
+  }, [circle0, getHue, onChange])
+
+  useEffect(() => {
+    if (!circle1) return
+
+    circle1.call(
+      d3
+        .drag()
+        .on('drag', ({ x, y }) => {
+          onChange((color) => ({
+            ...color,
+            hue: [color.hue[0], getHue(x)]
+          }))
+        })
+    )
+  }, [circle1, getHue, onChange])
+
+  useEffect(() => {
+    if (!bar) return
+
+    bar.call(
+      d3
+        .drag()
+        .on('drag', ({ x, y }) => {
+          onChange((color) => ({
+            ...color,
+            lightness: getLightness(y),
+          }))
+        })
+    )
+  }, [bar, getLightness, onChange])
+
+  useEffect(() => {
+    if (!color || !circle0) return
+
+    const x1 = getX(color.hue[0])
+    const x2 = getX(color.hue[1])
+    const y = getY(color.lightness)
+
+    circle0.attr('cx', x1).attr('cy', y)
+    circle1.attr('cx', x2).attr('cy', y)
+    bar.attr('x1', x1).attr('x2', x2).attr('y1', y).attr('y2', y)
+
+  }, [color, circle0, circle1, bar, getX, getY])
+
+  return (
+    <div id='double-hue-picker-container' className={classes.root} />
+  )
+}
+
+export default Pad
---
+++ client/src/components/core/DoubleHuePicker/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { hueGradient } from 'services/utils'
+import Pad from './Pad'
+
+const HUE_RANGE = [0, 360]
+const LIGHTNESS_RANGE = [0, 100]
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 10,
+    outline: '1px red solid',
+  },
+  pad: {
+    height: 200,
+    width: 250,
+    backgroundImage: `linear-gradient(0deg, hsla(0,0%,100%,1) 0%, hsla(0,0%,100%,0) 50%, hsla(0,0%,0%,0) 50%, hsla(0,0%,0%,1) 100%), ${hueGradient({
+      hueMin: HUE_RANGE[0],
+      hueMax: HUE_RANGE[1],
+      saturation: 100,
+      lightness: 50,
+      alpha: 1,
+      steps: 20,
+    })}`,
+  },
+  swatch: {
+    height: 30,
+    marginBottom: 20,
+    background: ({ color }) => hueGradient({
+      hueMin: color.hue[0],
+      hueMax: color.hue[1],
+      saturation: color.saturation,
+      lightness: color.lightness,
+      alpha: color.alpha,
+    }),
+  }
+}))
+
+const DoubleHuePicker = () => {
+  const [color, setColor] = useState({
+    hue: [50, 150],
+    saturation: 100,
+    lightness: 50,
+    alpha: 1.0,
+  })
+
+  const classes = useStyles({ color })
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.swatch} />
+      <div className={classes.pad}>
+        <Pad
+          color={color}
+          onChange={setColor}
+          hueRange={HUE_RANGE}
+          lightnessRange={LIGHTNESS_RANGE}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default DoubleHuePicker
