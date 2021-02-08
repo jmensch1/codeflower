b3e8a7bbf7f61337d3841a574fe5dd50f20dd856
Jake Mensch <jake.mensch@gmail.com>
2021-02-08 09:55:56 -0800

    delete gradient Backgrounds

--- client/src/components/core/GradientPicker/gradients/LightnessHue.js
+++ client/src/components/core/GradientPicker/gradients/LightnessHue.js
@@ -1,7 +1,4 @@
-import React from 'react'
-import tinycolor from 'tinycolor2'
-import { hueGradient, checkerGradient } from 'services/utils'
-import Background from 'components/core/Background'
+import { hueGradient } from 'services/utils'
 
 const lightnessGradient = ({ alpha = 1.0 }) => {
   return `
@@ -15,43 +12,25 @@ const lightnessGradient = ({ alpha = 1.0 }) => {
   `
 }
 
-const LightnessHue = ({
+const lightnessHue = ({
   lightnessRange = [0, 100],
   hueRange = [0, 360],
   saturation = 0,
   alpha = 1.0,
   backgroundColor = 'hsla(0, 0%, 0%, 1.0)',
-}) => {
-  const backgroundLightness = tinycolor(backgroundColor).toHsl().l * 100
-  return (
-    <Background layers={[
-      checkerGradient({
-        alpha: 0.008,
-        backgroundColor,
-      }),
-      lightnessGradient({
-        alpha,
-      }),
-      `
-        linear-gradient(
-          90deg,
-            hsla(0,0%,${backgroundLightness}%,1) 0%,
-            hsla(0,0%,${backgroundLightness}%,0) 50%,
-            hsla(0,0%,${backgroundLightness}%,0) 50%,
-            hsla(0,0%,${backgroundLightness}%,1) 100%
-        )
-      `,
-      hueGradient({
-        hueMin: hueRange[0],
-        hueMax: hueRange[1],
-        saturation,
-        lightness: 50,
-        alpha,
-        steps: 20,
-        direction: 'bottom',
-      }),
-    ]} />
-  )
-}
+}) => [
+  lightnessGradient({
+    alpha: 1,
+  }),
+  hueGradient({
+    hueMin: hueRange[0],
+    hueMax: hueRange[1],
+    saturation,
+    lightness: 50,
+    alpha,
+    steps: 20,
+    direction: 'bottom',
+  }),
+].join(',')
 
-export default LightnessHue
+export default lightnessHue
--- client/src/components/core/GradientPicker/gradients/SaturationHue.js
+++ client/src/components/core/GradientPicker/gradients/SaturationHue.js
@@ -1,53 +1,35 @@
-import React from 'react'
-import tinycolor from 'tinycolor2'
-import { hueGradient, checkerGradient } from 'services/utils'
-import Background from 'components/core/Background'
+import { hueGradient } from 'services/utils'
 
-const saturationGradient = ({ alpha }) => {
+const saturationGradient = ({ alpha, lightness }) => {
   return `
     linear-gradient(
       90deg,
-        hsla(0,0%,50%,${alpha}),
-        hsla(0,0%,50%,0)
+        hsla(0,0%,${lightness}%,${alpha}),
+        hsla(0,0%,${lightness}%,0)
     )
   `
 }
 
-const SaturationHue = ({
+const saturationHue = ({
   saturationRange = [0, 100],
   hueRange = [0, 360],
   lightness = 0,
   alpha = 1.0,
   backgroundColor = 'hsla(0, 0%, 0%, 1.0)',
-}) => {
-  const backgroundLightness = tinycolor(backgroundColor).toHsl().l * 100
-  return (
-    <Background layers={[
-      checkerGradient({
-        alpha: 0.008,
-        backgroundColor,
-      }),
-      saturationGradient({
-        alpha,
-      }),
-      `
-        linear-gradient(
-          90deg,
-            hsla(0,0%,${backgroundLightness}%,1),
-            hsla(0,0%,${backgroundLightness}%,0)
-        )
-      `,
-      hueGradient({
-        hueMin: hueRange[0],
-        hueMax: hueRange[1],
-        saturation: 100,
-        lightness,
-        alpha,
-        steps: 20,
-        direction: 'bottom',
-      }),
-    ]} />
-  )
-}
+}) => [
+  saturationGradient({
+    alpha: 1,
+    lightness,
+  }),
+  hueGradient({
+    hueMin: hueRange[0],
+    hueMax: hueRange[1],
+    saturation: 100,
+    lightness,
+    alpha,
+    steps: 20,
+    direction: 'bottom',
+  }),
+].join(',')
 
-export default SaturationHue
+export default saturationHue
