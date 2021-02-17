d31914c8199b4b5d6e9cb17bf269064f7578c3b5
Jake Mensch <jake.mensch@gmail.com>
2021-02-17 08:35:13 -0800

    add mixpanel to file endpoint

--- server/server/api/endpoints/file.js
+++ server/server/api/endpoints/file.js
@@ -1,12 +1,24 @@
 const fs = require('fs')
 const config = require('@config')
+const mixpanel = require('@util/mixpanel')
 
-module.exports = ({ repoId, path }) => {
+module.exports = async ({ repoId, path }) => {
   if (!repoId || !path) throw config.errors.BadFileRequest
 
   const absPath = config.paths.repo(repoId, 'root', path)
-  return fs.promises.readFile(absPath, 'utf-8').catch((err) => {
+
+  try {
+    const data = await fs.promises.readFile(absPath, 'utf-8')
+
+    mixpanel.track('file', {
+      distinct_id: 'user',
+      repoId,
+      path,
+    })
+
+    return data
+  } catch(err) {
     if (err.code === 'ENOENT') throw config.errors.FileNotFound
     else throw err
-  })
+  }
 }
