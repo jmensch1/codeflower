ca0cbd2ded4fe59b5875953bcc2a67bfc1d4b386
Jake Mensch <jake.mensch@gmail.com>
2021-02-16 17:44:14 -0800

    move web vitals reporting into services

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import history from 'services/history'
-import App from './components/App'
-import MainThemeProvider from './components/MainThemeProvider'
 import store from './store'
-//import reportWebVitals from './reportWebVitals'
-
-// console.log('environment:', process.env)
+import MainThemeProvider from './components/MainThemeProvider'
+import App from './components/App'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,8 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
