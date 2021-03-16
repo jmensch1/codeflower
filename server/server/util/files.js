8f005f41a596b087635d0e0b212e7f1b803b902f
Jake Mensch <jake.mensch@gmail.com>
2021-03-16 12:52:12 -0700

    fix for cloc escape-character issue

--- server/server/util/files.js
+++ server/server/util/files.js
@@ -5,7 +5,17 @@ function writeJson(path, data) {
 }
 
 function readJson(path) {
-  return fs.promises.readFile(path, 'utf-8').then(JSON.parse)
+  return fs.promises.readFile(path, 'utf-8')
+    /*
+      NOTE: this replacement fixes an invalid JSON error that arises when
+      cloc output includes a filename that contains an escape character.
+      e.g. https://github.com/systemd/systemd/blob/main/test/fuzz/fuzz-unit-file/dev-mapper-fedora_krowka%5Cx2dswap.swap
+
+      I made an issue in the cloc repo about this:
+      https://github.com/AlDanial/cloc/issues/575
+    */
+    .then(str => str.replace(/\\/g, '\\\\'))
+    .then(JSON.parse)
 }
 
 module.exports = {
