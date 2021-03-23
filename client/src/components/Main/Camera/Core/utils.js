7ebb1bffbbb56830e19d2aafdd84e43733e6bd10
Jake Mensch <jake.mensch@gmail.com>
2021-03-22 19:02:01 -0700

    firefox fix for getViewBoxAperture

--- client/src/components/Main/Camera/Core/utils.js
+++ client/src/components/Main/Camera/Core/utils.js
@@ -32,18 +32,19 @@ export function getAperture(containerRect, aspectRatio) {
 // converts the given aperture to the svg's viewBox coordinate system
 export function getViewBoxAperture(svg, aperture) {
   const { left, top, width, height } = aperture
-  const matrix = svg.getCTM().inverse()
+  const svgRect = svg.getBoundingClientRect()
+  const matrix = svg.getScreenCTM().inverse()
 
   // calc top-left corner of aperture
   const pt0 = svg.createSVGPoint()
-  pt0.x = left
-  pt0.y = top
+  pt0.x = left + svgRect.x
+  pt0.y = top + svgRect.y
   const svgPt0 = pt0.matrixTransform(matrix)
 
   // calc bottom-right corner
   const pt1 = svg.createSVGPoint()
-  pt1.x = left + width
-  pt1.y = top + height
+  pt1.x = left + width + svgRect.x
+  pt1.y = top + height + svgRect.y
   const svgPt1 = pt1.matrixTransform(matrix)
 
   return {
@@ -53,3 +54,30 @@ export function getViewBoxAperture(svg, aperture) {
     height: svgPt1.y - svgPt0.y,
   }
 }
+
+// // converts the given aperture to the svg's viewBox coordinate system
+// NOTE: this version is a bit cleaner but was causing an error in firefox
+// due to the call to getCTM()
+// export function getViewBoxAperture(svg, aperture) {
+//   const { left, top, width, height } = aperture
+//   const matrix = svg.getCTM().inverse()
+//
+//   // calc top-left corner of aperture
+//   const pt0 = svg.createSVGPoint()
+//   pt0.x = left
+//   pt0.y = top
+//   const svgPt0 = pt0.matrixTransform(matrix)
+//
+//   // calc bottom-right corner
+//   const pt1 = svg.createSVGPoint()
+//   pt1.x = left + width
+//   pt1.y = top + height
+//   const svgPt1 = pt1.matrixTransform(matrix)
+//
+//   return {
+//     left: svgPt0.x,
+//     top: svgPt0.y,
+//     width: svgPt1.x - svgPt0.x,
+//     height: svgPt1.y - svgPt0.y,
+//   }
+// }
