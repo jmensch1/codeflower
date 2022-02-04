457cc6849e598116f516a7cf0df1fc62dffcc12d
Jake Mensch <jake.mensch@gmail.com>
2022-02-04 04:51:24 -0800

    add github sha to ping output

--- server/server/api/endpoints/ping.js
+++ server/server/api/endpoints/ping.js
@@ -3,7 +3,11 @@ const Log = require('@log')
 module.exports = function servePing() {
   Log(1, 'Serving ping response.')
 
-  let data = { message: 'codeflower server is running' }
+  let data = { 
+    message: 'codeflower server is running',
+    githubSha: process.env.GITHUB_SHA || null,
+  }
+
   let hostName = process.env.HOSTNAME
   if (hostName) data.hostName = hostName
 
